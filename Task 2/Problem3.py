1cf62d278b11fedf92aa3904cc44253987bab251
NazmulHasan660 <nazmulhasanmahady992@gmail.com>
2026-06-09 16:22:33 +0600

    Add reverse_complement function to compute reverse complement

--- Task 2/Problem3.py
+++ Task 2/Problem3.py
@@ -1,1 +1,16 @@
+def reverse_complement(pattern):
+  complement_map = {'A': 'T', 'T': 'A', 'C': 'G','G': 'C'}
+
+  reversed_pattern = pattern[::-1]
+
+  rev_comp =""
+  for nucleotide in reversed_pattern:
+      rev_comp += complement_map[nucleotide]
+
+  return rev_comp
+
+pattern_input = "AAAACCCGGT"
+result = reverse_complement(pattern_input)
+
+print(result)
 
