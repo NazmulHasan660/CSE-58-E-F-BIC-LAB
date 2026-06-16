7944c632a12b945c2e0b6ce31a974ceee58b295f
NazmulHasan660 <nazmulhasanmahady992@gmail.com>
2026-06-16 21:07:48 +0600

    Implement function to find second maximum number

--- Task 1/YouDo1.py
+++ Task 1/YouDo1.py
@@ -1,5 +1,24 @@
+def find_second_maximum():
+    try:
+        user_input = input().strip()
+        
+        if not user_input:
+            print("-1")
+            return
+            
+        numbers = list(map(int, user_input.split()))
+        unique_numbers = set(numbers)
+        
+        if len(unique_numbers) < 2:
+            print("-1")
+        else:
+            sorted_numbers = sorted(list(unique_numbers), reverse=True)
+            print(sorted_numbers[1])
+            
+    except EOFError:
+        print("-1")
+    except ValueError:
+        print("-1")
 
-name = input("what is your favourite color ")
-
-
-print("Your favorite color is [" + name + "]")
+if __name__ == "__main__":
+    find_second_maximum()
